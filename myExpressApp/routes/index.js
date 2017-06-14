565edc3c11734dbb2d0eb73f44afe918b824aba9
Dennis Müller <CSTX_a_MUDENN@corpdir.onmicrosoft.com>
2017-06-14 10:52:56 +0200

    simple command input and button layout

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // res.send('<p>some html</p>');
+  res.sendFile(__dirname + '/public/index.html');
+
+  //res.render('index', { title: 'Express' });
+});
+
+var sys = require('sys')
+var exec = require('child_process').exec;
+router.post('/', function (req, res, next) {
+
+  function puts(error, stdout, stderr) { res.send(stdout); }
+  exec("ls -la", puts);
+
 });
 
 module.exports = router;
